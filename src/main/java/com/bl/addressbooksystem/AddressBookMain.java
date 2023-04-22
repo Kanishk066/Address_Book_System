a3e3f5e05f3d8ec89ef9cf99557c8c26aa86cef7
Kanishk <kanishksinghal06@gmail.com>
2023-04-22 10:39:36 +0530

    Added the code for adding new contacts

--- src/main/java/com/bl/addressbooksystem/AddressBookMain.java
+++ src/main/java/com/bl/addressbooksystem/AddressBookMain.java
@@ -3,5 +3,9 @@ package com.bl.addressbooksystem;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome To Address Book System Program");
+        AddressBookClass obj = new AddressBookClass();
+        obj.addContacts();
+        System.out.println(obj.display());
     }
 }
+
