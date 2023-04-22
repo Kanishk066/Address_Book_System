d683b8d9201dcad3856494084401156edf5d28bb
Kanishk <kanishksinghal06@gmail.com>
2023-04-22 07:29:39 +0530

    Added the Welcome Message

---
+++ src/main/java/com/bl/addressbooksystem/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.bl.addressbooksystem;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Address Book System Program");
+    }
+}
