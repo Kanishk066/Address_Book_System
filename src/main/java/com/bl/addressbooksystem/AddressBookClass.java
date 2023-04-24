1021533b99a010207c6953e43aecc6f9da3cddae
Kanishk <kanishksinghal06@gmail.com>
2023-04-24 14:47:35 +0530

    Added the code to Edit the Contacts

--- src/main/java/com/bl/addressbooksystem/AddressBookClass.java
+++ src/main/java/com/bl/addressbooksystem/AddressBookClass.java
@@ -1,5 +1,4 @@
 package com.bl.addressbooksystem;
-
 import java.util.Scanner;
 
 public class AddressBookClass {
@@ -29,7 +28,38 @@ public class AddressBookClass {
 
         System.out.println("Enter Email Address: ");
         obj.setEmailAddress(sc.next());
+    }
+    // Get a contact from the address book by name
+    Contacts getContactByName(String firstName, String lastName) {
+        if (obj.getFirstName().equals(firstName) && obj.getLastName().equals(lastName)) {
+            return obj;
+        } else {
+            return null;
+        }
+    }
+    // Edit an existing contact in the address book by name
+    void editContactByName(String firstName, String lastName) {
+        if (obj.getFirstName().equals(firstName) && obj.getLastName().equals(lastName)) {
+            System.out.println("Enter Address: ");
+            obj.setAddress(sc.next());
 
+            System.out.println("Enter City Name: ");
+            obj.setCity(sc.next());
+
+            System.out.println("Enter State Name: ");
+            obj.setState(sc.next());
+
+            System.out.println("Enter Zip Code: ");
+            obj.setZip(sc.nextInt());
+
+            System.out.println("Enter Phone Number: ");
+            obj.setPhoneNumber(sc.nextLong());
+
+            System.out.println("Enter Email Address: ");
+            obj.setEmailAddress(sc.next());
+        } else {
+            System.out.println("Contact not found.");
+        }
     }
     Contacts display()
     {
--- src/main/java/com/bl/addressbooksystem/AddressBookMain.java
+++ src/main/java/com/bl/addressbooksystem/AddressBookMain.java
@@ -1,11 +1,39 @@
 package com.bl.addressbooksystem;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome To Address Book System Program");
         AddressBookClass obj = new AddressBookClass();
-        obj.addContacts();
-        System.out.println(obj.display());
+        Scanner sc = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println("Enter your choice:");
+            System.out.println("1. Add Contact");
+            System.out.println("2. Display Contact");
+            System.out.println("3. Edit Contact");
+            System.out.println("4. Exit");
+
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1 -> obj.addContacts();
+                case 2 -> {
+                    Contacts contacts = obj.display();
+                    System.out.println(contacts.toString());
+                }
+                case 3 -> {
+                    System.out.println("Enter the First Name of Contact to Edit:");
+                    String firstName = sc.next();
+                    System.out.println("Enter the Last Name of Contact to Edit:");
+                    String lastName = sc.next();
+                    obj.editContactByName(firstName, lastName);
+                }
+                case 4 -> System.exit(0);
+                default -> System.out.println("Invalid choice");
+            }
+        } while (choice != 4);
     }
 }
 
